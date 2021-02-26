81707b126b804e67e254bdbb483d6fe1252af810
xiansliu <326427540@qq.com>
2021-02-26 14:34:52 +0800

    接收数据，JSON格式

--- src/main/java/com/simple/framework/protocol/http/HttpServerHandler.java
+++ src/main/java/com/simple/framework/protocol/http/HttpServerHandler.java
@@ -1,26 +1,34 @@
 package com.simple.framework.protocol.http;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.simple.framework.Invocation;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * HttpServer 服务
  */
 public class HttpServerHandler {
 
-
-    public void handler(HttpServletRequest req, HttpServletResponse resp) {
-
-        System.out.printf("服务处理");
-        System.out.printf(req.getRequestURI());
-        // 接收请求 处理逻辑
+    // 接收请求 处理逻辑
         /*
           1、接口名
           2、方法名
           3、方法参数类型列表
           4、方法值列表
          */
+    public void handler(HttpServletRequest req, HttpServletResponse resp) {
+        System.out.printf("服务处理");
+        System.out.printf(req.getRequestURI());
+        try {
+            Invocation invocation = JSONObject.parseObject(req.getInputStream(), Invocation.class);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
